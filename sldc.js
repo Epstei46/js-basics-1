0acf9dffe6ac9e2154825e6c465ff85f5e004956
Steven Epstein <sbepstein42@gmail.com>
2021-10-16 17:00:19 -0400

    Fixed Completed Project

--- sldc.js
+++ sldc.js
@@ -5,8 +5,8 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
-// Analysis of Requirements
 // Planning
+// Analysis of Requirements
 // Design
 // Implementation
 // Testing & Integration
@@ -17,9 +17,9 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
 
-let Requirements = "Analysis of Requirements"
-let Planning = "Planning"
-let Design = "Design"
-let Implementation = "Implementation"
-let Testing = "Testing & Integration"
-let Maintain = "Maintain"
+let Planning = "Project leaders get feedback from users/customers, set up the team structure, and set deadlines for goals"
+let Requirements = "Decide what the application needs to do, how it should work, and any other requirements, such as hardware it needs to work on (like different phones or a specific machine)"
+let Design = "Deciding how the software application will work, such as: which programming language to use, how user interface should look and respond to input, which platforms it should work on, any methods for solving problems and performing tasks in the application, how the application will communicate with other assets, and security measures for data on the app"
+let Implementation = "This is when coding of the application begins"
+let Testing = "Application is tested, in specific parts as well as a whole, and security testing. Once the application is done being tested, it can be made available (deployed) to the users/customers"
+let Maintain = "At this point the application has been made available to users/customers. If new bugs are found, they need to be maintained/resolved."
